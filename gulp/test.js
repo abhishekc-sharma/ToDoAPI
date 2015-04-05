66c8ad63e18a20768c115387f4fbaa2f3d0a7272
Abhishek C. Sharma <portgasd666@gmail.com>
2015-04-05 12:46:39 +0530

    Added coverage to gitignore Added .travis.yml file with coveralls support Removed test_coverage task from gulp and package.json
    Coverage will not be chekced locally but only on push

--- gulp/test.js
+++ gulp/test.js
@@ -6,20 +6,20 @@ var istanbul = require('gulp-istanbul');
 
 var isWatching = false;
 
-gulp.task('test_coverage', function(done) {
-  gulp.src(['./**/*.js', '!node_modules/**/*', '!test/**/*'])
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire())
-    .on('finish', function() {
-      gulp.src('./test/**/*.js')
-        .pipe(mocha())
-        .once('error', function() {
-          process.exit(1);
-        })
-        .pipe(istanbul.writeReports())
-        .on('end', done);
-    });
-});
+//gulp.task('test_coverage', function(done) {
+//  gulp.src(['./**/*.js', '!node_modules/**/*', '!test/**/*'])
+//    .pipe(istanbul())
+//    .pipe(istanbul.hookRequire())
+//    .on('finish', function() {
+//      gulp.src('./test/**/*.js')
+//        .pipe(mocha())
+//        .once('error', function() {
+//          process.exit(1);
+//        })
+//        .pipe(istanbul.writeReports())
+//        .on('end', done);
+//    });
+//});
 
 gulp.task('test', function(done) {
   return gulp.src(['./test/**/*.js'])
